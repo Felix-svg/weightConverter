8e53e5e2b93d5c16939fbabefd03937698d54814
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-02-17 23:32:23 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+weight = float(input("Enter your weight: "))
+unit = input("(K)g or (L)bs: ")
+
+if unit.upper() == "K":
+    converted_unit = weight / 0.45
+    print("Weight in lbs: " + str(converted_unit))
+elif unit.upper() == "L":
+    converted_unit = weight * 0.45
+    print("Weight in kgs: " + str(converted_unit))
+else:
+    print("Please enter a valid unit")
