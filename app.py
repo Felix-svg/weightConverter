7e305fb55649e9f8dcbc49ed31d2b051e5a3c733
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-02-18 09:17:24 +0300

    Floored the calculations

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import math
+
 weight = float(input("Enter your weight: "))
 unit = input("(K)g or (L)bs: ")
 
 if unit.upper() == "K":
-    converted_unit = weight / 0.45
+    converted_unit = math.floor(weight / 0.45)
     print("Weight in lbs: " + str(converted_unit))
 elif unit.upper() == "L":
-    converted_unit = weight * 0.45
+    converted_unit = math.floor(weight * 0.45)
     print("Weight in kgs: " + str(converted_unit))
 else:
     print("Please enter a valid unit")
